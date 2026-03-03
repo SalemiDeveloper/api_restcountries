5e0c57d2fab509b883f01608d20c3c3302578c1d
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2026-03-03 17:53:42 +0000

    Iniciando o projeto

---
+++ api_reestcountries/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// Definindo constante para controlar o fluxo da aplicação
+define('CONTROL', true);
+
+// Incluindo arquivos
+$routes = require_once('inc/route.php');
+
+// Definindo rotas
+$route = $_GET['route'] ?? 'home';
+
+if (!in_array($route, $routes)) {
+    $route = '404';
+}
+
+// Fluxo das rotas
+switch ($route) {
+    case 'home':
+        require_once 'inc/header.php';
+        require_once 'scripts/home.php';
+        require_once 'inc/footer.php';
+        break;
+    
+    case '404':
+        require_once 'inc/header.php';
+        require_once 'scripts/404.php';
+        require_once 'inc/footer.php';
+        break;
+}
+
